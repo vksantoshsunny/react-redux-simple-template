7d26ba78a2b42a63e987d53d13e3d6c0e0e3023c
vksantoshsunny <vksantoshsunny@gmail.com>
2019-04-07 12:39:47 +1200

    added grid styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import SimpleComponent from './components/simple-component'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import { connect } from 'react-redux';
 import { defaultFunction } from './actions';
@@ -14,11 +20,29 @@ class App extends Component {
   }
 
   render() {
+    const value = 0;
     return (
       <div>
-        React Redux Starter Template
-        <SimpleComponent/>
+      <div>
+      <Tabs
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+            centered
+          >
+            <Tab label="Item One" />
+            <Tab label="Item Two" />
+            <Tab label="Item Three" />
+            <Tab label="Item Four" />
+            <Tab label="Item Five" />
+            <Tab label="Item Six" />
+            <Tab label="Item Seven" />
+          </Tabs>
       </div>
+      {value === 0 && <SimpleComponent></SimpleComponent>}
+        </div>
     );
   }
 }
--- src/components/simple-component.js
+++ src/components/simple-component.js
@@ -1,11 +1,55 @@
 // basic react component starting template
 import React, { Component } from 'react';
 import Counter from './Counter'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button';
+
+
+
+
 
 class SimpleComponent extends Component {
     render() {
         return (
-            <div><Counter></Counter></div>
+    <Grid container spacing={24} justify="space-between" >
+
+        <Grid item xs={1}>
+
+        </Grid>
+        <Grid item xs={10}>
+          <Paper style={{padding : 20, marginTop: 20}} ></Paper>
+        </Grid>
+        <Grid item xs={1}>
+
+        </Grid>
+        <Grid item xs={1}>
+
+        </Grid>
+        <Grid item xs={3}>
+        <Button style={{marginRight : 20}} variant="contained" >
+        Default
+        </Button>
+        <Button style={{marginRight : 20}} variant="contained" >
+        Default
+        </Button>
+        </Grid>
+        <Grid item xs={4}>
+          
+        </Grid>
+        <Grid item xs={3}>
+        <Button style={{marginRight : 20}} variant="contained" >
+        Default
+        </Button>
+        <Button style={{marginRight : 20}} variant="contained" >
+        Default
+        </Button>
+        </Grid>
+        <Grid item xs={1}>
+
+        </Grid>
+
+      </Grid>
         );
     }
 }
